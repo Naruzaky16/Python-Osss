405cdfa80f97b2d507e5df174f32071a06702062
AdhityaNaruzaky <151916683+Naruzaky16@users.noreply.github.com>
2023-12-04 14:06:13 +0700

    Add files via upload

---
+++ perulangan.py
@@ -0,0 +1,37 @@
+# # #Perulangan For
+# for n in range(10,30,) :
+#     print(n)
+#     input("Shenggol Dongg")
+#     print("Manchester City 3 Point" , n)
+#     print("Manchester United 0 Point" , n)
+
+# # #Perulangan Manual
+# print("MAnchester United")
+# print("MAnchester United")
+# print("MAnchester United")
+# print("MAnchester United")
+# print("MAnchester United")
+# print("MAnchester United")
+# print("MAnchester United")
+# print("MAnchester United")
+# print("MAnchester United")
+
+# # #Perulangan String
+# pemain = "Jude Bellingham"
+# for huruf in pemain :
+#     print(huruf)
+
+# #Perulangan While
+# pilihan = input("apakah ripki ganteng? ya ayau tidak : ")
+
+# while(pilihan == "ya") :
+#     pilihan = input("apakah ripki ganteng? ya ayau tidak : ")
+
+
+# angka = 1
+
+# while(angka <= 10) :
+#     print(angka)
+#     angka = angka + 1
+
+# #break
